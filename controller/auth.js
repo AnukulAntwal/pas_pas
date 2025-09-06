d2bb78e1b3a6eda57e9f9c5d892329536b9c4e38
Anukul <anukulantwal@gmail.com>
2025-09-06 19:26:19 +0530

    bcrypt password

--- controller/auth.js
+++ controller/auth.js
@@ -1,9 +1,9 @@
 import User from "../models/User.js";
 import { loginValidate, registerValidate } from "../validations/userValidate.js";
+import bcrypt from "bcryptjs";
 
-// ✅ Login Controller
 export const loginController = async (req, res) => {
-  // 1. Validate request body
+  
   const { error } = loginValidate.validate(req.body);
   if (error)
     return res.status(400).json({ error: error.details[0].message });
@@ -17,12 +17,6 @@ export const loginController = async (req, res) => {
       return res.status(401).json({ error: "Invalid email or password" });
     }
 
-    // ⚡ If you want password hashing with bcrypt:
-    // const validPassword = await bcrypt.compare(password, user.password);
-    // if (!validPassword) {
-    //   return res.status(401).json({ error: "Invalid email or password" });
-    // }
-
     // 3. If matched → success response
     return res.status(200).json({
       success: true,
@@ -34,17 +28,21 @@ export const loginController = async (req, res) => {
   }
 };
 
-// ✅ Register Controller
+
 export const registerController = async (req, res) => {
-  // 1. Validate request body
+const hashPassword = async (password) => {
+  const hashedPass = await bcrypt.hash(password, 10);  
+  return hashedPass;
+};
   const { error } = registerValidate.validate(req.body);
   if (error)
     return res.status(400).json({ error: error.details[0].message });
 
   try {
-    const { email } = req.body;
-
-    // 2. Check if user already exists
+    const {first_name,last_name,phone_number,email,password}=req.body
+     const hpassword = await hashPassword(password) 
+     console.log(hpassword);
+     
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res
@@ -52,14 +50,9 @@ export const registerController = async (req, res) => {
         .json({ error: "User already exists with this email" });
     }
 
-    // ⚡ If you want password hashing:
-    // const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
     // 3. Create new user
-    const newUser = new User({
-      ...req.body,
-      // password: hashedPassword   // if bcrypt used
-    });
+
+    const newUser = new User({first_name,last_name,phone_number,email,hpassword });
 
     await newUser.save();
 
--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
     },
-    password: {
+    hpassword: {
       type: String,
       required: true,
       minlength: 5,

0bc4d95216d5f11af390920c65518efbeb27be97
Anukul <kanukul@teqmavens.com>
2025-10-24 10:28:42 +0530

    changes notification

--- controller/notificationController.js
+++ controller/notificationController.js
@@ -41,7 +41,7 @@ export const sendMessageNotification = async (req, res) => {
 
 export const getNotifications = async (req, res) => {
   try {
-    const { user_id } = req.body;
+    const { user_id } = req.query;
     if (!user_id) {
       return res.status(400).json({
         status: "fail",

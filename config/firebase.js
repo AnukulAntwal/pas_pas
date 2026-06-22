838c23e5823f76e9d6be40a2e0404e8b75d70ce2
Anukul <anukulantwal@gmail.com>
2026-06-22 21:17:09 +0530

    file name changes

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 import serviceAccount from "./firebase-service-account.json" with { type: "json" };
-
+//firebase-service-account.json is a file that contains the service account credentials for your Firebase project. It is used to authenticate your server with Firebase services. Make sure to keep this file secure and do not expose it publicly.
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });

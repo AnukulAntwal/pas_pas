6016341d18a4f498eb8d0456a8aff4b7b92df35c
Anukul <anukulantwal@gmail.com>
2026-07-22 22:14:57 +0530

    changes notifcatio n

--- controller/packageController.js
+++ controller/packageController.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import Package from "../models/Package.js";
 import moment from "moment-timezone";
+import { sendPushNotification } from "../utils/notification.js";
 import {
   getRoadStops,
   filterStopsBetween,
